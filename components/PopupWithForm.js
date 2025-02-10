e440e4891fbbacb5f080ebffbbb90eff0cd4e175
Kuma_Lord <eddison.torres@gmail.com>
2025-02-09 20:40:16 -0500

    Added components to project, popup/popform/counter/sections. created loose coupling for interactivity.

---
+++ components/PopupWithForm.js
@@ -0,0 +1,29 @@
+import Popup from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  _getInputValues() {
+    const inputs = Array.from(
+      this._popupForm.querySelectorAll(".popup__input")
+    );
+    const inputValues = {};
+    inputs.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupForm.addEventListener("submit", (evt) => {
+      this._handleFormSubmit(this._getInputValues());
+      evt.preventDefault();
+    });
+  }
+}
+export default PopupWithForm;
